b1b6aea2c301fddef08fc1e23ec0263bd5e9613b
AdixSasuke <adityakhandelwal0271@gmail.com>
2025-02-17 16:15:58 +0530

    Refactor Two Sum solution to use a hashmap for improved efficiency

--- 1.two-sum.java
+++ 1.two-sum.java
@@ -5,16 +5,32 @@
  */
 
 // @lc code=start
+
+import java.util.*;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int [] res = new int [2];
+        // int [] res = new int [2];
+        // for(int i = 0; i < nums.length; i++){
+        //     for(int j = i + 1; j < nums.length; j++){
+        //         if(nums[i]+nums[j] == target){
+        //             res = new int[] {i, j};
+        //             return res;
+        //         }
+        //     }
+        // }
+        // return null;
+
+        Map<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i < nums.length; i++){
-            for(int j = i + 1; j < nums.length; j++){
-                if(nums[i]+nums[j] == target){
-                    res = new int[] {i, j};
-                    return res;
-                }
+            int curr = nums[i];
+            //targer = curr + x
+            //x = target - curr
+            int x = target - curr;
+            if(map.containsKey(x)){
+                return new int[] {map.get(x), i};
             }
+            map.put(curr, i);
         }
         return null;
     }
