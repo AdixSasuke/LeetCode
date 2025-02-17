a6d703487fb6a195f57387ec15281246645e4bbb
AdixSasuke <adityakhandelwal0271@gmail.com>
2025-02-17 15:47:23 +0530

    Added solution for Two Sum problem in Java

---
+++ 1.two-sum.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=1 lang=java
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int [] res = new int [2];
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i + 1; j < nums.length; j++){
+                if(nums[i]+nums[j] == target){
+                    res = new int[] {i, j};
+                    return res;
+                }
+            }
+        }
+        return null;
+    }
+}
+// @lc code=end
+
