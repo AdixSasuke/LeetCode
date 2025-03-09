b65348d1e2c1f17b053e801e18a00b83407f7215
AdixSasuke <adityakhandelwal0271@gmail.com>
2025-03-09 17:00:51 +0530

    Refactor majorityElement solution to use insertion sort for improved clarity

--- 169.majority-element.java
+++ 169.majority-element.java
@@ -1,15 +1,26 @@
-import java.util.Arrays;
-
 /*
  * @lc app=leetcode id=169 lang=java
  *
  * [169] Majority Element
  */
-
+import java.util.Arrays;
 // @lc code=start
 class Solution {
     public int majorityElement(int[] nums) {
-        //using sort
+        // int majorityElement = nums.length / 2;
+        // for(int i: nums){
+        //     int count = 0;
+        //     for(int j: nums){
+        //         if(i == j){
+        //             count++;
+        //         }
+        //     }
+        //     if(count>majorityElement){
+        //         return i;
+        //     }
+        // }
+        // Insertion sort on nums array
+//using sort 
         Arrays.sort(nums);
         return nums[nums.length / 2];
     }
