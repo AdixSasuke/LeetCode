b46e958b52e3bec6d06fac85b6193478b45a4d09
AdixSasuke <adityakhandelwal0271@gmail.com>
2025-06-02 15:56:10 +0530

    Add solution for Cells with Odd Values in a Matrix problem and Find Numbers with Even Numbers of Digits in Java

---
+++ 1252.cells-with-odd-values-in-a-matrix.java
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=1252 lang=java
+ *
+ * [1252] Cells with Odd Values in a Matrix
+ */
+
+// @lc code=start
+class Solution {
+    public int oddCells(int m, int n, int[][] indices) {
+        int result = 0;
+        int[][] matrix = new int[m][n];
+        for(int i = 0; i < matrix.length; i++){
+            
+        }
+        return result;
+    }
+}
+// @lc code=end
+
---
+++ 1295.find-numbers-with-even-number-of-digits.java
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=1295 lang=java
+ *
+ * [1295] Find Numbers with Even Number of Digits
+ */
+
+// @lc code=start
+class Solution {
+    public int findNumbers(int[] nums) {
+        int result = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(hasEvenDigits(nums[i])){
+                result++;
+            }
+        }
+        return result;
+    }
+    
+    public boolean hasEvenDigits(int num){
+        boolean result = false;
+        int count = 0;
+        while(num > 0){
+            count++;
+            num = num/10;
+        }
+        if(count % 2 == 0){
+            result = true;
+        }
+        return result;
+    }
+}
+// @lc code=end
+
